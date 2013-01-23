c0c8c5ee462070a4a27537d7f817e9a65c29e244
Cairnarvon <cairnarvon@gmail.com>
2013-01-23 04:28:47 +0100

    Another version bump.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='uptime',
-    version='1.1.1',
+    version='1.2.0',
     description='Cross-platform uptime library',
     long_description='''\
 This module provides a cross-platform way to retrieve system uptime.
