e08faccf2113eedeb161422aa3af83020d19dff3
Cairnarvon <cairnarvon@gmail.com>
2013-01-27 08:47:13 +0100

    libroot seems like a common library name.
    I'm aware of at least one other project using the name, so let's not break
    if someone has that installed.

--- src/__init__.py
+++ src/__init__.py
@@ -71,6 +71,9 @@ def _uptime_beos():
     except:
         return None
 
+    if not hasattr(libroot, 'system_time'):
+        return None
+
     libroot.system_time.restype = ctypes.c_int64
     return libroot.system_time() / 1000000.
 
